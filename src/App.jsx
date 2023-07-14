bb71a8940fdc394681b95828bc05c15ef655616e
Mac <131044845+mmcdv@users.noreply.github.com>
2023-07-14 14:48:35 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Store from './pages/Store';
-import Success from './pages/Success';
 import Product from './pages/Product';
 import { CartProvider } from './CartContext';
 import { CssBaseline, Box, Container } from '@mui/material';
@@ -17,8 +16,6 @@ function App() {
           <Container>
             <Routes>
               <Route index element={<Store />} />
-              <Route path="success" element={<Success />} />
-              <Route path="cancel" element={<Cancel />} />
               <Route path="product/:id" element={<Product />} />
             </Routes>
         </Container>
